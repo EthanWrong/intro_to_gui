a34f9870f8e7bf24f0d254eda2c3b100bdc5e006
WronglyInspired <wonge2@middleton.school.nz>
2023-03-07 12:04:59 +1300

    GUI and repetition support

--- spelling_converter.py
+++ spelling_converter.py
@@ -1,5 +1,5 @@
 """Converts NZ English string to US English string by checking over
-individual spellings of words"""
+spelling patterns in words"""
 from easygui import *
 
 
@@ -11,12 +11,28 @@ SPELLINGS = {
 }
 
 
-nz_input = input("Enter your sentence in NZ English")
-us_output = nz_input
-for i in SPELLINGS:
-    if i in us_output:
-        us_output = us_output.replace(i, SPELLINGS[i])
-if us_output == nz_input:
-    print("No change in spelling")
-else:
-    print(us_output)
+def main():
+    nz_input = enterbox("Enter your sentence in NZ English", "Enter Sentence")
+    us_output = nz_input
+
+    # for every pattern in SPELLINGS, check if it is in sentence,
+    # and replace that part of the string if so
+    for i in SPELLINGS:
+        if i in us_output:
+            us_output = us_output.replace(i, SPELLINGS[i])
+            
+    if us_output == nz_input:
+        msgbox("No change in spelling", "Output")
+    else:
+        msgbox(f"New sentence in US English:\n\n{us_output}", "Output")
+
+    if ynbox("Would you like to convert another sentence?", "Repeat "
+                                                            "Conversion"):
+        main()
+    else:
+        msgbox("Goodbye", "End program")
+
+
+main()
+
+
